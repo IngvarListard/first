0bad36d642f1509e5d39cca11ea8164b6884eba8
IngvarListard <vlzvoice@ya.ru>
2018-03-27 01:14:37 +0300

    adding0.2

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 token = '486658164:AAEKCXMICg9R1njGG_hCSLAsGyNl0rC_p-c'
-eedb_file = '/home/ingvar/PycharmProjects/HogeBot/ee_database.json'
-pedb_file = '/home/ingvar/PycharmProjects/HogeBot/pe_database.json'
+eedb_file = 'ee_database.json'
+pedb_file = 'pe_database.json'
